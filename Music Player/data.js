a1911fdf0cde07434711efbab75e3c482c266d37
Sakshi Kumari <124695612+SakshiKumari24@users.noreply.github.com>
2025-07-29 21:43:17 +0530

    Add files via upload

---
+++ Music Player/data.js
@@ -0,0 +1,34 @@
+let songs=[
+    {
+        name: "Dreams",
+        artist: "Bensound",
+        path : "Music/bensound-dreams.mp3",
+        cover : "Image/cover 1.png",
+    },
+    {
+        name: "Going Higher",
+        artist: "Bensound",
+        path : "Music/bensound-goinghigher.mp3",
+        cover : "Image/cover 2.png"
+    },
+    {
+        name: "Tere Sang",
+        artist: "80sElectricmusicandlyrics",
+        path : "Music/tere-sang-with-you-207644.mp3",
+        cover : "Image/cover 3.png"
+    },
+    {
+        name: "Heartbroken Winter",
+        artist: "SrinathKoushik",
+        path : "Music/heartbroken-winter-sad-instrumental-bollywood-song-306095.mp3",
+        cover : "Image/cover 4.png"
+    },
+    {
+        name: "Teri Ye Adaa",
+        artist: "prazkhanal",
+        path : "Music/teri-ye-adaa-romantic-song-206526.mp3",
+        cover : "Image/cover 5.png"
+        
+    },
+    
+];
